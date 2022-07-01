52b351d57f2fbe61edc6fd3742034f18f97e587b
charles.dong <charles.dong@cth.group>
2022-07-01 14:09:44 +0800

    feat: map api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
-import apiconfig from "./baseconfig";
 import service from "./request";
 
 export const getAreaData = (): Promise<any> => {
-  return service.get(apiconfig.URL_BASE + apiconfig.URL_AREA);
+  return service.get('');
 };
